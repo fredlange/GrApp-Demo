6665952f475de85d08c2adf769fab64293a78cde
Fredrik Lange <fredrik.lange@bisnode.com>
2021-01-15 11:00:32 +0100

    Some simple customer creation

--- banking/customer/src/customer.resolvers.ts
+++ banking/customer/src/customer.resolvers.ts
@@ -1,9 +1,25 @@
 import { Graphlet } from '@graphle/graphlet';
 import gql from 'graphql-tag';
 
+
+const DummyCustomers = {}
+
 export const root = {
-  Query: () => {
-    customer: () => {};
-    findCustomer: () => {}
-  },
+
+    customer: (args) => {
+      return DummyCustomers[args.id]
+    },
+    findCustomer: (args) => {
+      return []
+    },
+    createCustomer: (args) => {
+      const generatedSimpleId = Date.now()
+      DummyCustomers[generatedSimpleId] = {
+        id: generatedSimpleId,
+        name: args.name
+      }
+
+    return DummyCustomers[generatedSimpleId]
+    }
+
 };
--- banking/customer/src/customer.source.ts
+++ banking/customer/src/customer.source.ts
@@ -1,12 +1,15 @@
 import gql from "graphql-tag";
 
-export const source = gql`
+export const source = `
 type Customer {
   id: String!
   name: String!
 }
 type Query {
   customer(id: String!): Customer
-  findCustomers(name: String): Customer[]
+  findCustomers(name: String): [Customer]
+}
+type Mutation {
+  createCustomer(name: String!): Customer
 }
 `;
